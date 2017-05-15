ec1b0b89154ad483af97eaa7bf2ab9aff73cd266
litaf <litafkupfer@gmail.com>
2017-05-15 17:10:26 +0300

    123

---
+++ Project/src/main/java/testing/CreateSequenceFile.java
@@ -0,0 +1,34 @@
+package testing;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import Project.TrainingData.Protein;
+
+public class CreateSequenceFile {
+	
+	private ArrayList<Protein> m_proteinsDB;
+	public CreateSequenceFile(ArrayList<Protein> proteinsDB){
+		m_proteinsDB = proteinsDB;
+		create();
+	}
+	
+	private void create() {
+		try{
+		    PrintWriter writer = new PrintWriter("proteinDB.txt", "UTF-8");
+		    for (Protein protein : m_proteinsDB) {
+		    	writer.println(String.format("> %d %s", protein.getProteinIndex(), protein.getAminoAcids()));
+
+			}
+
+		   writer.close();
+		} catch (IOException e) {
+		  
+		}
+		
+	}
+	
+	
+
+}
