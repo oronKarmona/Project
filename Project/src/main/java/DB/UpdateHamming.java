6f8b4f24d33f1042274c64187058921d8b10a5ad
Oron <Oron@172.16.10.2>
2017-05-16 13:56:51 +0300

    d

--- Project/src/main/java/DB/UpdateHamming.java
+++ Project/src/main/java/DB/UpdateHamming.java
@@ -2,6 +2,7 @@ package DB;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -14,12 +15,19 @@ public class UpdateHamming
 	private ArrayList<Protein> proteinsdb ; 
 	private Map<String, Object> map ;  
 	ElasticSearchService es ;
-	
+	private Map<Integer , Integer> indexMap;
 	public UpdateHamming(ArrayList<Protein> proteinsdb) throws IOException, InterruptedException, ExecutionException
 	{
 		es = new ElasticSearchService();
+		indexMap = new HashMap<Integer,Integer>();
 		int ctr = 0 ;
 		this.proteinsdb = proteinsdb;
+		
+		for(int i = 0 ; i< proteinsdb.size() ; i++)
+		{
+			indexMap.put(proteinsdb.get(i).getProteinIndex(), i);
+		}
+			
 		int firstP , secondP , firstF, secondF , hamming;
 		String  fragmentA = "", fragmentB = "";
 		for(int i= 1 ; i <= 205933669; i++ )
@@ -35,24 +43,28 @@ public class UpdateHamming
 							secondP = (Integer)map.get("secondProteinIndex");
 							secondF = (Integer)map.get("secondFragmentIndex");
 							
-							
-								for(Protein p : proteinsdb)
-								{
-										if(p.getProteinIndex() == firstP)
-										{
-											fragmentA = p.GetFragments(firstF);
-										}
-											
-										 if(p.getProteinIndex() == secondP)
-										{
-											fragmentB = p.GetFragments(secondF);
-										}
-								}
+							fragmentA = proteinsdb.get(indexMap.get(firstP)).GetFragments(firstF);
+							fragmentB = proteinsdb.get(indexMap.get(secondP)).GetFragments(secondF);
+//								for(Protein p : proteinsdb)
+//								{
+//										if(p.getProteinIndex() == firstP)
+//										{
+//											fragmentA = p.GetFragments(firstF);
+//										}
+//											
+//										 if(p.getProteinIndex() == secondP)
+//										{
+//											fragmentB = p.GetFragments(secondF);
+//										}
+//								}
 						
-									
+							
 							hamming = this.hamming(fragmentA , fragmentB);
 							
 							es.updateDocument(hamming , i );
+							
+							if(i%100 == 0)
+								System.out.println(i);
 					}
 					
 					else
