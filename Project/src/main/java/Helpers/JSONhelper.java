1a19983141efd3998ea1a6ca7f55e4df0b72408c
Oron <Oron@ORON>
2017-06-03 11:38:21 +0300

    d

--- Project/src/main/java/Helpers/JSONhelper.java
+++ Project/src/main/java/Helpers/JSONhelper.java
@@ -66,7 +66,8 @@ public class JSONhelper
 	        	
 	        	for(int i = 0 ; i < amount ; i ++)
 	        	{
-		            Object obj = parser.parse(new FileReader(FileName+i+".json"));
+		            @SuppressWarnings("resource")
+					Object obj = parser.parse(new FileReader(FileName+i+".json"));
 		 
 		            JSONArray jsonArray = (JSONArray) obj;
 		            
