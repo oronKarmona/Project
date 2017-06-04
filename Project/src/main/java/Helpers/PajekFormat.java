009fb18d6714b78c69490aa7700082c6cfac13c8
Oron <Oron@ORON>
2017-06-04 10:22:14 +0300

    pajek file format

---
+++ Project/src/main/java/Helpers/PajekFormat.java
@@ -0,0 +1,33 @@
+package Helpers;
+
+import java.util.ArrayList;
+
+import DB.ElasticSearchService;
+import PCN.Neighbors;
+
+public class PajekFormat 
+{
+		private ElasticSearchService es ; 
+		private ArrayList<Neighbors> graph;
+		
+		
+		public PajekFormat(String cluster_es_index , String cluster_es_type)
+		{
+			this.es = new ElasticSearchService(cluster_es_index, cluster_es_type);
+			graph = new ArrayList<Neighbors>();
+			this.retreive_graph();
+		}
+		
+		
+		
+		private void retreive_graph()
+		{
+			long number_of_vertex = es.getCountOfDocInType() ;
+			
+			for(int i = 0 ; i < number_of_vertex ; i++)
+			{
+				graph.add(es.getNeighbors(i));
+			}
+				
+		}
+}
