bd7ef7d3170d400dda19028280dbee13f8443796
litaf <litafkupfer@gmail.com>
2017-06-08 12:23:57 +0300

    LinearSystemSolution1

---
+++ Project/src/main/java/Project/TrainingData/LinearSystemSolution.java
@@ -0,0 +1,13 @@
+package Project.TrainingData;
+
+import java.util.ArrayList;
+
+public class LinearSystemSolution {
+
+	
+	public LinearSystemSolution(ArrayList<Protein> proteinsList){
+		
+	}
+	
+	
+}
