d79f8cbcac81abc7eec71d599865d36ab2313e07
litaf <litafkupfer@gmail.com>
2017-06-22 14:00:54 +0300

    add meanRmsd array to neighbor

--- Project/src/main/java/Calculation/WeightFunctionCalculation.java
+++ Project/src/main/java/Calculation/WeightFunctionCalculation.java
@@ -2,14 +2,40 @@ package Calculation;
 
 import java.util.List;
 
+import DB.ElasticSearchService;
 import PCN.Node;
+import PCN.NodePCN;
 import ProGAL.geom3d.Point;
 
 public class WeightFunctionCalculation {
 
-	public static double Calculate(int clusterNum, double miu){
+	private ElasticSearchService m_cluterClient;
+	private double m_miu;
+
+	public WeightFunctionCalculation(int clusterNum, double miu){
 		
-		return 0;
+		m_miu = miu;
+		m_cluterClient = new ElasticSearchService("cluster", clusterNum+"");
+		Calculate();
+
+	}
+	public void Calculate(){
+		
+		NodePCN node = new NodePCN();
+		
+		int count =(int) (m_cluterClient.getCountOfDocInType() -2); 
+		
+		for(int i=0;i<count;i++){
+			
+			node = m_cluterClient.getVertexAt(i);
+			
+			for (Node neighbor : node.getNeighbors()) {
+				
+			}
+			
+		}
+
+	
 	}
 	
 	
